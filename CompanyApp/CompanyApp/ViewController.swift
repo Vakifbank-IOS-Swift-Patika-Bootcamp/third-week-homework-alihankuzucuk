8173dc4912d3e86b417c5a862e11aadbb5ef6570
Alihan KUZUCUK <95879997+alihankuzucuk@users.noreply.github.com>
2022-11-15 12:17:36 +0300

    CompanyApp project has been created

---
+++ CompanyApp/CompanyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompanyApp
+//
+//  Created by Alihan KUZUCUK on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
